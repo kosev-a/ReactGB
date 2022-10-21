237ce080db48bdb8cc6ee8463dd6c09f660ebf2a
Alex_Koss <cahek27@mail.ru>
2022-10-21 17:58:27 +0300

    HW2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { Message } from './components/Message';
+import { Form } from './components/Form';
 
 export const App = () => {
-  const myMess = 'Homework #1';
-  const topPosition = '150px';
-  return (
-    <div className="App" style={{marginTop: topPosition}}>
-      <Message  mess={myMess} />
-    </div>
-  );
+  return <>
+    <div className='App' style={{ marginTop: '20px' }}>Список сообщений</div>
+    <Form />
+  </>
 }
